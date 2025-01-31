7ce1a554eb330deeafbbf5adef8a86ddb425d387
nullifidianz <nullifidianz@gmail.com>
2025-01-31 00:04:06 -0300

    update validate_user.go

---
+++ src/config/Logs/logs.go
@@ -0,0 +1,30 @@
+package logs
+
+import (
+	"log"
+	"os"
+)
+
+func InitLogs() {
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(file)
+}
+
+func CloseLogs() {
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file.Close()
+}
+
+func GetLogs() *os.File {
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return file
+}
